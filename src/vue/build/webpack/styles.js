6b7613e1e443d592e7f96d861889dfc08c1e3a05
Nicolas Husson <hello@nusson.ninja>
2019-03-09 00:13:43 -0500

    fix -m loader

--- src/vue/build/webpack/styles.js
+++ src/vue/build/webpack/styles.js
@@ -45,10 +45,10 @@ export default function (opts = {}) {
     {
       test: /\.css$/,
       use: cssLoaders,
-      include: [
-        WebpackHelper.DIRS.SRC,
-        ...options.includeCSSPaths,
-      ],
+      // include: [
+      //   WebpackHelper.DIRS.SRC,
+      //   ...options.includeCSSPaths,
+      // ],
     },
     // stylus
     (({ stylus }) => {
