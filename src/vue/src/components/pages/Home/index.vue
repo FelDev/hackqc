af66aaf6914d0bff8a2acad9e97de7a667ee1e12
Olivier Gauthier-Farley <olivier.gauthier-farley.1@ens.etsmtl.ca>
2019-03-08 23:32:09 -0500

    form wip

--- src/vue/src/components/pages/Home/index.vue
+++ src/vue/src/components/pages/Home/index.vue
@@ -6,34 +6,36 @@
 </docs>
 
 <script>
-import { mapGetters, mapState } from 'vuex';
-import NewsCard from 'components/pages/News/Card';
-import store from './store';
+import { mapGetters, mapState } from "vuex";
+import NewsCard from "components/pages/News/Card";
+import store from "./store";
+import sectionReport from "./report";
 
 export default {
-  name: 'PageHome',
+  name: "PageHome",
   components: {
     NewsCard,
+    sectionReport
   },
   computed: {
     ...mapGetters({
-      title: 'PageHome/title',
-      intro: 'PageHome/intro',
-      news: 'PageHome/news',
+      title: "PageHome/title",
+      intro: "PageHome/intro",
+      news: "PageHome/news"
     }),
-    ...mapState('I18n', {
-      locale: ({ locale }) => locale,
-    }),
+    ...mapState("I18n", {
+      locale: ({ locale }) => locale
+    })
   },
   /**
    * Register page store and load datas if needed
    */
   beforeCreate() {
     if (!this.$store.state.PageHome) {
-      this.$store.registerModule('PageHome', store);
+      this.$store.registerModule("PageHome", store);
     }
-    this.$store.dispatch('PageHome/LOAD');
-  },
+    this.$store.dispatch("PageHome/LOAD");
+  }
   // beforeDestroy() {
   //   // !!! Do not unregister store
   //   // or datas will be deleted and you will need to re-call api
@@ -44,30 +46,7 @@ export default {
 
 <template>
   <div class="PageHome _container">
-    <header class="header">
-      <h1
-        class="title"
-        v-html="title" />
-      <div
-        class="wysiwyg"
-        v-html="intro" />
-    </header>
-    <section class="section-news">
-      <header>
-        <h2
-          v-t="'PAGE_HOME.featured_news_title'"
-          class="subtitle" />
-      </header>
-
-      <ul class="list">
-        <li
-          v-for="article in news"
-          :key="`article-${article.id}`"
-          class="item">
-          <NewsCard v-bind="article" />
-        </li>
-      </ul>
-    </section>
+    <sectionReport/>
   </div>
 </template>
 
@@ -87,32 +66,41 @@ export default {
 </i18n>
 
 <style lang="stylus" scoped>
-  /**
-   * TABLE OF CONTENT
-   * --------
-   *  - LAYOUT
-   *  - DEBUG
-   *  - NO_SCOPE
-   */
+/**
+ * TABLE OF CONTENT
+ * --------
+ *  - LAYOUT
+ *  - DEBUG
+ *  - NO_SCOPE
+ */
+.title {
+  f-style(title, h1);
+  vertical-padding();
+}
 
-  //  ===LAYOUT===
-  .title
-    f-style(title, h1)
-    vertical-padding()
-  .subtitle
-    f-style(title)
-    vertical-margin()
+.subtitle {
+  f-style(title);
+  vertical-margin();
+}
 
-  .section-news
-    margin-top 2em
-    .list
-      flexbox(row, $align: stretch)
-      >.item
-        flex-basis 49%
-        +mobile()
-          flex-basis 100%
+.section-news {
+  margin-top: 2em;
 
-  //  ===DEBUG===
-  [data-debug-mode="true"] .PageHome
-    //
+  .list {
+    flexbox(row, $align: stretch);
+
+    >.item {
+      flex-basis: 49%;
+
+      +mobile() {
+        flex-basis: 100%;
+      }
+    }
+  }
+}
+
+// ===DEBUG===
+[data-debug-mode='true'] .PageHome {
+  // 
+}
 </style>
