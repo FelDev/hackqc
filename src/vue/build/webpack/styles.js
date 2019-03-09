e0839be3bcf99ab879556d9c16f7d58191c7b429
Nicolas Husson <hello@nusson.ninja>
2019-03-09 01:57:16 -0500

    fix -m kffein-stylus

--- src/vue/build/webpack/styles.js
+++ src/vue/build/webpack/styles.js
@@ -83,7 +83,7 @@ export default function (opts = {}) {
         options: {
           stylus: {
             import: [ // inject our settings stylus + libs
-              `${WebpackHelper.DIRS.NODE_MODULES}kffein-stylus/lib/vars/index`,
+              `${WebpackHelper.DIRS.ASSETS}styles/kffein-stylus/lib/vars/index`,
               `${WebpackHelper.DIRS.ASSETS}styles/settings/index`,
               `${WebpackHelper.DIRS.NODE_MODULES}kffein-stylus/index`,
             ],
