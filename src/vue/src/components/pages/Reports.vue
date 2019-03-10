6e9c7f2380e247345126757f12584781d7ebb38a
mesieu <felixparadis@gmail.com>
2019-03-09 19:44:34 -0500

    report is beautiful

--- src/vue/src/components/pages/Reports.vue
+++ src/vue/src/components/pages/Reports.vue
@@ -41,6 +41,7 @@ export default {
       },
       sent : false,
       fileSubmitted : false,
+      filePreviews: [],
       center: { lat: 45.508, lng: -73.587 },
       useCustomLocation : false,
       hasSetLocation : false,
@@ -56,150 +57,267 @@ export default {
     },
     photoSubmitted : function(){
       this.fileSubmitted = true
+
     },
     setLocation : function(bool){
-      if(bool){
-        this.useCustomLocation = false;
-        this.hasSetLocation = true;
-      }else{
-         this.useCustomLocation = true;
-         this.hasSetLocation = false;
-      }
+      navigator.geolocation.getCurrentPosition(position => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+      this.report.position.lat = position.coords.latitude
+      this.report.position.lng = position.coords.longitude
+      this.hasSetLocation = true;
+      });
+      this.hasSetLocation = true;
     },
     addMarker: function (event){
       this.report.position.lat = event.latLng.lat()
       this.report.position.lng = event.latLng.lng()
       this.hasSetLocation = true;
       console.log(this.report)
-    },
+    }
   }
 };
 </script>
 
 <template>
+
   <div class="sectionReport">
-    <h1>Signaler</h1>
+    <h1 v-if="!sent">Signaler</h1>
     <h2></h2>
     <div v-if="!sent" class="formContainer">
       <div class="categorie">
-        <label>Catégorie de l'incident</label>
-        <select v-model="report.categorie">
-          <option v-bind:value="categorie = 'inondations'">Inondations</option>
-          <option v-bind:value="categorie = 'crues'">crues</option>
-          <option v-bind:value="categorie = 'pluies'">pluies</option>
-          <option v-bind:value="categorie = 'insectes'">insectes</option>
-        </select>
-        <div v-if="report.categorie ==='insectes'">
-          <label>Espèces :</label>
-          <select v-model="report.especes">
-            <option v-bind:value="especes = 'punaises'">Punaises</option>
-            <option v-bind:value="especes = 'frelons'">Frelons</option>
+        <div class="subCatFlex">
+          <label>Catégorie de l'incident</label>
+          <select v-model="report.categorie">
+            <option disabled selected hidden>Faire un choix</option>
+            <option v-bind:value="categorie = 'inondations'">Inondations</option>
+            <option v-bind:value="categorie = 'crues'">crues</option>
+            <option v-bind:value="categorie = 'pluies'">pluies</option>
+            <option v-bind:value="categorie = 'insectes'">insectes</option>
           </select>
         </div>
+        <transition name="expand">
+          <div class="subCatFlex" v-if="report.categorie ==='insectes'" transition>
+            <label>Espèces</label>
+            <select v-model="report.especes">
+              <option v-bind:value="especes = 'punaises'">Punaises</option>
+              <option v-bind:value="especes = 'frelons'">Frelons</option>
+            </select>
+          </div>
+        </transition>
       </div>
-      <div class="date">
+      <div class="date subCatFlex">
         <label>Date de l'incident</label>
-        <datepicker :value="report.date"></datepicker>
+        <datepicker :value="report.date" :input-class="'dateInput'"></datepicker>
       </div>
-      <div class="criticite">
-        <label>Criticité de l'incident</label>
-          <input type="radio" id="one" value="1" v-model="report.criticite">
-          <label for="one">1</label>
-          <input type="radio" id="two" value="2" v-model="report.criticite">
-          <label for="two">2</label>
-          <input type="radio" id="two" value="3" v-model="report.criticite">
-          <label for="two">3</label>
-          <input type="radio" id="two" value="4" v-model="report.criticite">
-          <label for="two">4</label>
-          <input type="radio" id="two" value="5" v-model="report.criticite">
-          <label for="two">5</label>
+      <div class="criticite subCatFlex">
+        <label for="myRange">Sévérité</label>
+        <div class="slidecontainer">
+          <input type="range" min="1" max="5" value="3" class="slider" id="myRange" name="myRange" v-model="report.criticite">
+          <p>{{report.criticite}}</p>
+        </div>
       </div>
-      <div class="googlemaps">
+      <div class="googlemaps subCat">
         <label>Position de l'incident</label><br>
-        <button v-on:click="setLocation(true)">Position courante</button>
-        <button v-on:click="setLocation(false)">Ajouter une localisation</button>
-         <div v-if="hasSetLocation">Position Enregistrée</div>
-         <gmap-map v-if="useCustomLocation" :center="center" :zoom="12" style="width:100%;  height: 400px;" :options="{disableDefaultUI:true}" 
+        <div class="btnWrapper">
+          <button class="btn" v-on:click="setLocation(true)">Utiliser votre position actuelle</button>
+        </div>
+         <gmap-map :center="center" :zoom="12" style="width:100%;  height: 400px;" :options="{disableDefaultUI:true}" 
                 :clickable="true"
                 :draggable="true"
                 @click="addMarker">
                 <GmapMarker
                   :key="1"
                   :position="report.position"
-                  v-if="hasSetLocation"
                 />
         </gmap-map>
       </div>
-      <div class="description">
+      <div class="description subCat">
         <span>Informations supplémentaires</span>
-        <input type="text" v-model="report.description" >
+        <textarea type="text" v-model="report.description" />
       </div>
-      <div class="camera">
-        <input type="file" name="myFile" 
-       v-on:change="photoSubmitted()">
-        <div v-if="fileSubmitted" class="fas fa-check">Votre photo à été enregistré avec succès</div>
-      </div>
-      <div class="date">
-        <label for="checkbox" >Je veux un suivi sur mon incidence</label>
-        <input type="checkbox" id="checkbox" v-model="suivit">
-      </div>
-        <div v-if="suivit" class="suivit" >
-          <div class="nom">
-            <span>Votre Nom</span>
-            <input type="text" v-model="report.infoCitoyen.nom" >
-          </div>
-          <div class="email">
-            <span>Votre Email</span>
-            <input type="text" v-model="report.infoCitoyen.email" >
-          </div>
-          <div class="emailconf">
-            <span>Votre Email (Confirmation)</span>
-            <input type="text" v-model="report.infoCitoyen.emailConf" >
-          </div>
-          <div class="telephone">
-            <span>Votre Telephone</span>
-            <input type="text" v-model="report.infoCitoyen.telephone" >
-          </div>
+      <div class="camera subCat">
+        <label for="myFile">Ajouter un fichier (optionel)</label>
+        <input class="funkyInput" type="file" name="myFile"
+          v-on:change="photoSubmitted()">
+        <div v-if="fileSubmitted" class="fas fa-check">Votre photo à été enregistré avec succès
+          <!-- TODO it'be nice to have a preview -->
+          <!-- <div class="page" v-bind="filePreviews" :key="index" v-for="img in filePreviews">
+            <img :src="`/static/img/${pic}`">
+          </div> -->
         </div>
       </div>
-      <button v-if="!sent" class="btn" v-on:click="submit()">Envoyé</button>
+      <div class="suivitOrNot subCat">
+        <h2>Je veux un suivi sur mon incident</h2>
+        <div id="suivitOptions">
+          <input type="radio" id="suivitOui" name="suivit" value="oui" v-model="suivit">
+          <label for="suivitOui" >Oui svp!</label>
+          <input type="radio" id="suivitNon" name="suivit" value="non" v-model="suivit">
+          <label for="suivitNon" >Non, merci.</label>
+        </div>
+      </div>
+        <transition name="expand">
+          <div v-if="suivit=='oui'" class="suivit">
+            <div class="nom subCatFlex">
+              <label for="name">Votre Nom</label>
+              <input class="funkyInput" name="name" type="text" v-model="report.infoCitoyen.nom" >
+            </div>
+            <div class="email subCatFlex">
+              <label for="email">Votre Email</label>
+              <input class="funkyInput" name="email" type="text subCatFlex" v-model="report.infoCitoyen.email" >
+            </div>
+            <div class="emailconf subCatFlex">
+              <label for="emailConf">Votre Email (Confirmation)</label>
+              <input class="funkyInput" name="emailConf" type="text" v-model="report.infoCitoyen.emailConf" >
+            </div>
+            <div class="telephone subCatFlex">
+              <label for="phone">Votre Telephone</label>
+              <input class="funkyInput" name="phone" type="text" v-model="report.infoCitoyen.telephone" >
+            </div>
+          </div>
+        </transition>
+      </div>
+      <div class="btnWrapper subCat">
+        <button v-if="!sent" class="btn" v-on:click="submit()">Envoyé</button>
+      </div>
       <div v-if="sent">Merci beaucoup de votre collaboration! Vos élus sont impatient de lire vos messages. En attendant vous pouvez aller
         faire un tour sur le site de portail de données québec!
-      <router-link class="logo" :to="{name: `home`}">
-        <button class="btn">Retour au menu principal</button>
-      </router-link>
-
-        
-      </div>
-      
-      </div>
+        <router-link class="logo" :to="{name: `home`}">
+          <div class="btnWrapper subCat">
+            <button class="btn">Retour au menu principal</button>
+          </div>
+        </router-link>
+    </div>
+  </div>
    
 </template>
 
 <style lang="stylus" scoped>
-
-input[type='file'] {
-  color: transparent;
-}
-
-h1 {
-  color: red;
-  font-size: 5em;
-  font-family: "Times New Roman", Times, serif;
-  font-weight: bold;
-  text-align: left;
-
-}
-
-  /**
-  * TABLE OF CONTENT
-  * --------
-  *  - LAYOUT
-  *  - DEBUG
-  *  - NO_SCOPE
-  */
-
   //  ===LAYOUT===
+  .btnWrapper
+    text-align center
+    .btn
+      width 80vw
 
-  //  ===DEBUG===
+  .expand-enter-active, .expand-leave-active {
+    transition: all .3s ease
+    height: inherit
+    // padding: 10px
+    background-color: #eee
+    overflow: hidden
+  }
+  .expand-enter, .expand-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+  .subCat, .subCatFlex
+    margin-top: 1em;
+  .subCatFlex  
+    display flex
+    justify-content space-between
+    flex-direction column
+  h1
+    color: red
+    font-size: 5em
+    font-family: "Times New Roman", Times, serif
+    font-weight: bold
+    text-align: center
+
+  input[type='file']
+    color: #FFF
+
+  input[type="radio"]
+    visibility:hidden
+    width:0
+    height:0
+
+  label
+    font-size 1.1em
+
+  select
+    background: $c-inputColor
+    border-radius: 1em
+    border 2px solid darken($c-inputColor, 10%)
+    text-align-last: center
+    box-shadow: 0 0 black
+    color: #FFF
+    padding: 0.4em
+    font-weight: bold
+    font-size 1.1em
+    min-width 10em
+
+  textarea
+    width: 100%
+    min-height: 3em
+
+  .suivitOrNot 
+    & input[type="radio"]
+      position: absolute
+      opacity: 0
+      height: 0
+      width: 0
+      &:checked + label
+        color black
+        background-color $c-success
+        &:after
+          content '√'
+    & #suivitOptions
+      display flex
+      justify-content: space-around
+      display flex
+    & label
+      background-color: darken($c-success,10)
+      border-radius 1em
+      color #CCC
+      padding 0.5em 1em 0.5em 0.5em
+  
+  .slidecontainer
+    width: 100%;
+
+  .slider
+    -webkit-appearance: none;
+    width: 100%;
+    height: 15px;
+    border-radius: 5px;
+    background: #d3d3d3;
+    background-image: linear-gradient(90deg, rgba(14,249,21,1) 0%, rgba(255,0,0,1) 100%);
+    outline: none;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+
+  .slider:hover
+    opacity: 1;
+
+  .slider::-webkit-slider-thumb
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    border: 2px solid $c-inputColor;
+    background: #111;
+    cursor: pointer;
+
+  .slider::-moz-range-thumb
+    width: 25px;
+    height: 25px;
+    border: 2px solid $c-inputColor;
+    border-radius: 50%;
+    background: #111;
+    cursor: pointer;
+
+</style>
+
+<style lang="stylus">
+  .dateInput, .funkyInput
+    background: $c-inputColor
+    width: 100%
+    border-radius: 1em
+    border 2px solid darken($c-inputColor, 10%)
+    padding: 0.4em
+    color: #fff
+    text-align: center
+    font-weight: bold
+    font-size: 1.1em
 </style>
